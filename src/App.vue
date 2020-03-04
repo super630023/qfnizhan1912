f8a9a696701ff04f6c1e3b85a018243b50b18c8f
unknown <630041069@qq.com>
2020-03-04 17:08:37 +0800

    第二次提交

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <tabbar v-show="isTabbarShow"></tabbar>
+    <!-- 路由容器(基于slot封装) -->
+    <transition name="app" mode="out-in">
+        <router-view></router-view>
+    </transition>
+  </div>
+</template>
+<script>
+import tabbar from '@/components/Tabbar'
+import { mapState } from 'vuex'
+export default {
+  mounted () {
+    console.log(this.isTabbarShow)
+  },
+  computed: {
+    // 从tabbar命名空间中直接找 isTabbarShow
+    ...mapState('tabbar', ['isTabbarShow'])
+  },
+  components: {
+    tabbar
+  }
+}
+</script>
+<style lang="scss">
+  *{
+    margin:0;
+    padding: 0;
+  }
+  ul{
+    list-style:none;
+  }
+
+  html{
+    // touch-action: none;
+  }
+
+  .app-enter-active {
+    animation: aaa .5s;
+  }
+  .app-leave-active {
+    animation: aaa .5s reverse;
+  }
+  @keyframes aaa {
+    0% {
+      opacity: 0;
+      transform: translateY(100px);
+    }
+
+    100% {
+      opacity: 1;
+      transform: translateX(0px);
+    }
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router' // ./router/index
+import store from './store'
+import '@/util/filter' // 引入全局过滤器
+import '@/util/directive' // 引入全局指令
+import '@/util/component' // 引入全局组件
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App) // app根组件
+}).$mount('#app')
