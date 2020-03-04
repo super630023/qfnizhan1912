49dd5492fd4feff1f11484d7264b9b154509914c
huan1917 <1219767380@qq.com>
2020-03-04 17:37:26 +0800

    kong

--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+     111111111111
     <swiper :key="looplist.length">
       <div class="swiper-slide" v-for="data in looplist" :key="data.bannerId">
         <img :src="data.imgUrl"/>
